6541adefb6e1fd498ed64a7602927009bd3ed19a
SashaBohdanov <oleksandr.bohdanov.ik.2023@lpnu.ua>
2023-11-01 21:07:19 +0200

    first master commit

---
+++ PR5.5/PR5.5.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int Bin(int k, int n, int level, int& depth)
+{
+    if (level > depth)
+        depth = level;
+    cout << " level = " << level << endl;
+    if (k == n || k == 0)
+        return 1;
+    if (n > 0)
+        return Bin(k - 1, n - 1, level + 1, depth) + Bin(k, n - 1, level + 1, depth);
+}
+
+int main()
+{
+    int depth, k, n;
+    depth = 0;
+
+    cout << "k = "; cin >> k;
+    cout << "n = "; cin >> n; cout << endl;
+    cout << endl << "   Bin = " << Bin(k, n, 1, depth) << endl;
+    cout << "   recursive depth = " << depth << endl;
+}
+
